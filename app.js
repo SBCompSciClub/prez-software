c02cd13153f055e1a669482f8e7ededafa6401e4
Sethu Senthil <sethunarayansenthil@gmail.com>
2018-09-28 23:17:33 -0400

    Almost completed lesson 1 beginner prez and much more
    Added gitgnore,
    Almost completed lesson 1 beginner prez,
    Security Tweaks,
    Fresh Controls UI ,
    New font

--- app.js
+++ app.js
@@ -4,21 +4,33 @@ const express = require('express'),
       port = process.env.PORT || 8081,
 	  io = require('socket.io').listen(app.listen(port));
 
-app.get('/controler', (req, res) => {
-	res.sendFile(`${__dirname}/public/controls.html`);
+app.get('/adv/controler/:dynamicroute', function(req,res) {
+	res.sendFile(`${__dirname}/public/slides/advanced/day${req.params.dynamicroute}/controls.html`);
 });
-app.get('/slides/:dynamicroute', function(req,res) {
-	res.sendFile(`${__dirname}/public/slides/day${req.params.dynamicroute}/viewer.html`);
+app.get('/adv/:dynamicroute', function(req,res) {
+	res.sendFile(`${__dirname}/public/slides/advanced/day${req.params.dynamicroute}/viewer.html`);
 });
-app.get('/slides/sudo/:dynamicroute', function(req,res) {
-	res.sendFile(`${__dirname}/public/slides/day${req.params.dynamicroute}/index.html`);
+app.get('/adv/sudo/:dynamicroute', function(req,res) {
+	res.sendFile(`${__dirname}/public/slides/advanced/day${req.params.dynamicroute}/index.html`);
 });
+app.get('/beg/controler/:dynamicroute', function(req,res) {
+	res.sendFile(`${__dirname}/public/slides/beginners/day${req.params.dynamicroute}/controls.html`);
+});
+app.get('/beg/:dynamicroute', function(req,res) {
+	res.sendFile(`${__dirname}/public/slides/beginners/day${req.params.dynamicroute}/viewer.html`);
+});
+app.get('/beg/sudo/:dynamicroute', function(req,res) {
+	res.sendFile(`${__dirname}/public/slides/beginners/day${req.params.dynamicroute}/index.html`);
+});
+
 
 //path temp fixes
 app.get('/css/print/pdf.css', (req, res) => {
 	res.sendFile(`${__dirname}/public/assets/css/print/pdf.css`);
 });
-
+app.get('/beg/sudo/plugin/highlight/highlight.js', (req, res) => {
+	res.sendFile(`${__dirname}/public/assets/revealjs/plugin/highlight/highlight.js`);
+});
 app.use(express.static(__dirname + '/public'));
 
 //the password
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,79 +1,84 @@
+
 $(function() {
+	for (let i = 0; i < document.getElementsByClassName('link'); i++) {
+		document.getElementsByClassName('link')[i].addEventListener('click', function(){
+			window.open('https://'+this.innerText);
+		});
+	}
+	for (let i = 0; i < document.getElementsByClassName('email'); i++) {
+		document.getElementsByClassName('email')[i].href = 'mailto:'+this.innerText;
+		window.open('https://'+this.innerText);
+	}
+  Reveal.initialize({
+    history: true,
+	fragmentInURL: true,
+	dependencies: [
+		{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+	]
+  });
+  let socket = io();
+  let form = $("form.login");
+  let secretTextBox = form.find("input[type=password]");
+  let presentation = $(".reveal");
 
-	Reveal.initialize({
-		history: true
-	});
+  let key = "",
+    animationTimeout;
 
-	let socket = io();
-	let form = $('form.login');
-	let secretTextBox = form.find('input[type=text]');
-	let presentation = $('.reveal');
+  form.submit(function(e) {
+	e.preventDefault();
 
-	let key = "", animationTimeout;
-
-	form.submit(function(e){
-
-		e.preventDefault();
-
-		key = secretTextBox.val().trim();
-
-		if(key.length) {
-			socket.emit('load', {
-				key: key
-			});
+	hasher = (k)=>{
+		output = 1;
+		for(let i=0;i<k.length;i+=3){
+		output *= ((0x6B**parseInt(k.substring(i, i+3)))%0x38D7E58F3CB5F)%1000;
+		}
+		return output;
 		}
 
-	});
+    if (sudo && secretTextBox.val().trim() ==='javascript' /*hasher() = */) {
+		key = 'kittens';
+    } else if (!sudo) {
+      key = secretTextBox.val().trim();
+    }
+    if (key.length) {
+      socket.emit("load", {
+        key: key
+      });
+    }
+  });
 
+  socket.on("access", function(data) {
+    if (data.access === "granted") {
+      // Unblur everything
+      presentation.removeClass("blurred");
+      form.hide();
+	  let ignore = false;
+      $(window).on("hashchange", function() {
+        if (ignore) {
+          return;
+        } else if (sudo) {
+          let hash = window.location.hash;
+          socket.emit("slide-changed", {
+            hash: hash,
+            key: key
+          });
+        }
+      });
 
-	socket.on('access', function(data){
-
-
-		if(data.access === "granted") {
-
-			// Unblur everything
-			presentation.removeClass('blurred');
-
-			form.hide();
-
-			let ignore = false;
-
-			$(window).on('hashchange', function(){
-
-				if(ignore){
-					return;
-				} else if(sudo){
-					let hash = window.location.hash;
-					//changesslide to current every one sec
-					setInterval(function(){
-						socket.emit('slide-changed', {
-							hash: hash,
-							key: key
-						});
-					},1000)
-				}
-			});
-
-			socket.on('navigate', function(data){
-				window.location.hash = data.hash;
-				ignore = true;
-				setInterval(function () {
-					ignore = false;
-				},100);
-			});
-
-		}
-		else {
-
-			clearTimeout(animationTimeout);
-			secretTextBox.addClass('denied animation');
-			animationTimeout = setTimeout(function(){
-				secretTextBox.removeClass('animation');
-			}, 1000);
-
-			form.show();
-		}
-
-	});
-
+      socket.on("navigate", function(data) {
+        window.location.hash = data.hash;
+        ignore = true;
+        setInterval(function() {
+          ignore = false;
+        }, 100);
+      });
+    } else {
+      clearTimeout(animationTimeout);
+      secretTextBox.addClass("denied animation");
+      animationTimeout = setTimeout(function() {
+        secretTextBox.removeClass("animation");
+      }, 1000);
+      form.show();
+    }
+  });
 });
